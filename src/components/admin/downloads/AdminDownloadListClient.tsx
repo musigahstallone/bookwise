47c98a0a072357f924efc54a5806b18cb42ee92f
stallone odhiambo <odhiambostallone73@gmail.com>
2025-06-10 06:48:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/downloads/AdminDownloadListClient.tsx
+++ src/components/admin/downloads/AdminDownloadListClient.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useEffect } from 'react';
 import { type DownloadWithDetails } from '@/lib/tracking-service-firebase';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
